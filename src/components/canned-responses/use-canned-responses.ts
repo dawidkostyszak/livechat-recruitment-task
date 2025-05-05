e84d73eac5bd93c6c1a48281d3f710699e0640f0
Dawid Kostyszak <d.kostyszak@gmail.com>
2025-05-05 11:00:23 +0200

    fix: move debounce to useEffect, and cancel during re-renders

--- src/components/canned-responses/use-canned-responses.ts
+++ src/components/canned-responses/use-canned-responses.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { debounce } from 'lodash';
 import { setSearchAction } from '../../store/actions';
@@ -6,11 +6,15 @@ import { setSearchAction } from '../../store/actions';
 export const useCannedResponses = () => {
   const [search, setSearch] = useState('');
   const dispatch = useDispatch();
-  const debouncedDispatch = debounce(dispatch, 500);
+  const debouncedDispatch = debounce(() => dispatch(setSearchAction(search)), 1000);
+
+  useEffect(() => {
+    debouncedDispatch();
+    return () => debouncedDispatch.cancel();
+  }, [debouncedDispatch]);
 
   const handleSearch = (value: string) => {
     setSearch(value);
-    debouncedDispatch(setSearchAction(value));
   };
 
   return {
